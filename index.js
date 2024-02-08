3ad5111c1647d85dc5ff9b6ef395a62711040b9e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-08 23:41:16 +0530

    install cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan((tokens, req, res) => {
     return [
