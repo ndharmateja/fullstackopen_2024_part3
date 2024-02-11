4c00e2af6182f41a719137a5260cc72c084f3c2c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-02-11 16:26:35 +0530

    add eslint and fix errors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const Person = require("./models/Person");
 const mw = require("./middleware");
+const process = require("process");
 
 const app = express();
 
@@ -14,52 +15,50 @@ app.use(mw.requestLogger);
 
 // Routes
 app.route("/info").get((_req, res) => {
-  Person.countDocuments({}).then((count) => {
-    const dateString = new Date().toString();
-    return res.send(
-      `<p>Phonebook has info for ${count} people</p><p>${dateString}</p>`
-    );
-  });
+    Person.countDocuments({}).then((count) => {
+        const dateString = new Date().toString();
+        return res.send(
+            `<p>Phonebook has info for ${count} people</p><p>${dateString}</p>`
+        );
+    });
 });
 
-app
-  .route("/api/persons")
-  .get((_req, res) => {
-    Person.find({}).then((persons) => res.json(persons));
-  })
-  .post((req, res, next) => {
-    const { name, number } = req.body;
+app.route("/api/persons")
+    .get((_req, res) => {
+        Person.find({}).then((persons) => res.json(persons));
+    })
+    .post((req, res, next) => {
+        const { name, number } = req.body;
 
-    const newPerson = new Person({ name, number });
-    newPerson
-      .save()
-      .then((p) => res.json(p))
-      .catch((e) => next(e));
-  });
+        const newPerson = new Person({ name, number });
+        newPerson
+            .save()
+            .then((p) => res.json(p))
+            .catch((e) => next(e));
+    });
 
-app
-  .route("/api/persons/:id")
-  .get((req, res, next) => {
-    Person.findById(req.params.id)
-      .then((p) => (p ? res.json(p) : res.status(404).end()))
-      .catch((e) => next(e));
-  })
-  .delete((req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-      .then(() => res.status(204).end())
-      .catch((e) => next(e));
-  })
-  .put((req, res, next) => {
-    const { name, number } = req.body;
-    console.log("number:", number);
-    Person.findByIdAndUpdate(
-      req.params.id,
-      { name, number },
-      { new: true, runValidators: true, context: "query" }
-    )
-      .then((p) => (p ? res.json(p) : res.status(404).end()))
-      .catch((e) => next(e));
-  });
+app.route("/api/persons/:id")
+    .get((req, res, next) => {
+        Person.findById(req.params.id)
+            .then((p) => (p ? res.json(p) : res.status(404).end()))
+            .catch((e) => next(e));
+    })
+    .delete((req, res, next) => {
+        Person.findByIdAndDelete(req.params.id)
+            .then(() => res.status(204).end())
+            .catch((e) => next(e));
+    })
+    .put((req, res, next) => {
+        const { name, number } = req.body;
+        console.log("number:", number);
+        Person.findByIdAndUpdate(
+            req.params.id,
+            { name, number },
+            { new: true, runValidators: true, context: "query" }
+        )
+            .then((p) => (p ? res.json(p) : res.status(404).end()))
+            .catch((e) => next(e));
+    });
 
 //Unknown route
 app.use(mw.notFoundRoute);
--- models/Person.js
+++ models/Person.js
@@ -1,40 +1,41 @@
 const mongoose = require("mongoose");
+const process = require("process");
 
 const mongoDbUri = process.env.MONGODB_URI;
 mongoose
-  .connect(mongoDbUri)
-  .then((_result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    .connect(mongoDbUri)
+    .then((_result) => {
+        console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message);
+    });
 
 const personSchema = mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return /^\d{2,3}-[0-9|_]+/.test(v) || /^[0-9]+$/.test(v);
-      },
-      message: (props) => `${props.value} is not a valid phone number!`,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
     },
-  },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-[0-9|_]+/.test(v) || /^[0-9]+$/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+    },
 });
 
 personSchema.set("toJSON", {
-  transform: (_document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+    transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
 module.exports = mongoose.model("Person", personSchema);
